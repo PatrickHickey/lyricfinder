b8169e4d279a1feada16109214a38e051091d04a
PatrickHickey <codewithpat@gmail.com>
2018-10-26 00:29:30 -0700

    Edited lyrics.js

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -15,8 +15,16 @@ class Lyrics extends Component {
         }&apikey=${process.env.REACT_APP_MM_KEY}`
       )
       .then(res => {
-        console.log(res.data);
-        //this.setState({ track_list: res.data.message.body.track_list });
+        this.setState({ lyrics: res.data.message.body.lyrics });
+
+        return axios.get(
+          `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.get?track_id=${
+            this.props.match.params.id
+          }&apikey=${process.env.REACT_APP_MM_KEY}`
+        );
+      })
+      .then(res => {
+        this.setState({ track: res.data.message.body.track });
       })
       .catch(err => console.log(err));
   }
