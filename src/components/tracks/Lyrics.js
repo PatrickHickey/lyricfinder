266c25fb244437152f4aa23827d03fac134ec3ec
PatrickHickey <codewithpat@gmail.com>
2018-10-27 22:19:32 -0700

    Added lyric info

--- src/components/tracks/Lyrics.js
+++ src/components/tracks/Lyrics.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
 import axios from 'axios';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Spinner from '../layout/Spinner';
 
 class Lyrics extends Component {
   state = {
@@ -30,11 +32,32 @@ class Lyrics extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <h1>Lyrics</h1>
-      </div>
-    );
+    const { track, lyrics } = this.state;
+    if (
+      track === undefined ||
+      lyrics === undefined ||
+      Object.keys(track).length === 0 ||
+      Object.keys(lyrics).length === 0
+    ) {
+      return <Spinner />;
+    } else {
+      return (
+        <React.Fragment>
+          <Link to="/" className="btn btn-dark btn-sm mb-4">
+            Go Back
+          </Link>
+          <div className="class">
+            <h5 className="card-header">
+              {track.track_name} by{' '}
+              <span className="text-secondary">{track.artist_name}</span>
+            </h5>
+            <div className="card-body">
+              <p className="card-text">{lyrics.lyrics_body}</p>
+            </div>
+          </div>
+        </React.Fragment>
+      );
+    }
   }
 }
 
